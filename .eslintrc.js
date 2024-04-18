2047131d65ad9d1437b0c3f80c0d843f67ce7f68
joona0306 <dev.joona0306@gmail.com>
2024-04-18 18:00:43 +0900

    import/export 문제 해결 중

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,11 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "import"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "no-unused-vars": "off",
+    "import/no-unresolved": "error", // 예시로 추가한 규칙
   },
 };
